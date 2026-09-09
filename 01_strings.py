4261ed852c0b6d5f149bce772364121f271f711f
Tahreem-riaz <totehreem101@gmail.com>
2026-09-09 16:02:21 +0500

    Add the program no 1

--- 01_strings.py
+++ 01_strings.py
@@ -9,3 +9,10 @@ Topics: Strings, String Methods, Escape Sequences
 # ==========================================
 # PART A: STRING BASICS
 # ==========================================
+
+# Q1. Create three strings representing:
+#     - A book title
+#     - An author's name
+#     - A short description
+#
+#     Print all three strings.
