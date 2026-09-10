1f245cb4334597f9b939ad5d96c1a10be16f1e6c
Tahreem-riaz <totehreem101@gmail.com>
2026-09-10 21:19:35 +0500

    Add teh program no 1

--- 03_string_functions.py
+++ 03_string_functions.py
@@ -9,3 +9,9 @@ Topics: String Functions and String Operations
 # ==========================================
 # PART A: LENGTH & CHARACTER INFORMATION
 # ==========================================
+
+# Q1. Create a string containing the name of a
+#     programming language.
+#
+#     Find and print the number of characters in it
+#     using len().
