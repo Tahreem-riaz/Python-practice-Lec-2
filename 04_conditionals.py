241fd7ba726c3d3677cf03518780f8a8cb97cece
Tahreem-riaz <totehreem101@gmail.com>
2026-09-12 22:28:20 +0500

    Solve the program no 7

--- 04_conditionals.py
+++ 04_conditionals.py
@@ -128,3 +128,14 @@ else:
 #
 #     or:
 #     "Invalid Credentials"
+
+correct_username = "student"
+correct_password = "python123"
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == correct_username and password == correct_password:
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
