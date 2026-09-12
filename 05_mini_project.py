297626e664176c9ac8890eddc15eea5446866e39
Tahreem-riaz <totehreem101@gmail.com>
2026-09-12 22:33:29 +0500

    Add the heading of file 5 which is a mini project

---
+++ 05_mini_project.py
@@ -0,0 +1,16 @@
+"""
+========================================================
+          LECTURE 02 - FILE 5: MINI PROJECT
+========================================================
+Project: Username & Password Validator
+
+Concepts Used:
+- Strings
+- String Methods
+- Escape Sequences
+- Indexing
+- Slicing
+- String Functions
+- Conditional Statements
+========================================================
+"""
